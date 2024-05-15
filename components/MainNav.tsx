bbf34901a4c50da0e5a46552882afc87214caa3c
Michael Byers <mikebyers24@gmail.com>
2024-05-15 18:05:42 -0400

    styling navbar active link.

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,16 +1,34 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 import { ModeToggle } from './ModeToggle';
+import { usePathname } from 'next/navigation';
 
 const MainNav = () => {
+  const pathname = usePathname();
+  const links = [
+    { title: 'Dashboard', path: '/' },
+    { title: 'Tickets', path: '/tickets' },
+    { title: 'Users', path: '/users' },
+  ];
+
   return (
     <nav className="flex flex-col items-center border-b p-4">
       <div className="max-w-6xl w-full">
         <div className="flex justify-between">
           <div className="flex gap-4 items-center">
-            <Link href="/">Dashboard</Link>
-            <Link href="/tickets">Tickets</Link>
-            <Link href="/users">Users</Link>
+            {links.map((link) => (
+              <Link
+                key={link.title}
+                className={`nav-link ${
+                  pathname === link.path ? 'font-bold cursor-default' : ''
+                }`}
+                href={link.path}
+              >
+                {link.title}
+              </Link>
+            ))}
           </div>
           <div className="flex gap-4 items-center">
             <Link href="/">Login</Link>
