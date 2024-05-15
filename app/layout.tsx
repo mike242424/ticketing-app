cbebade661e13054a66c33ee86e67daef9dc6802
Michael Byers <mikebyers24@gmail.com>
2024-05-15 16:54:49 -0400

    light / dark modes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import MainNav from '@/components/MainNav';
+import { ThemeProvider } from '@/components/providers/theme.provder';
 
 export const metadata: Metadata = {
   title: 'Ticketing App',
@@ -13,12 +14,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <MainNav />
-        <main className="flex flex-col items-center p-4">
-          <div className="max-w-6xl w-full">{children}</div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <MainNav />
+          <main className="flex flex-col items-center p-4">
+            <div className="max-w-6xl w-full">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
 import React from 'react';
+import { ModeToggle } from './ModeToggle';
 
 const MainNav = () => {
   return (
     <nav className="flex flex-col items-center border-b p-4">
       <div className="max-w-6xl w-full">
         <div className="flex justify-between">
-          <div className="flex gap-4">
+          <div className="flex gap-4 items-center">
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
             <Link href="/users">Users</Link>
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-4 items-center">
             <Link href="/">Login</Link>
-            <Link href="/">Dark</Link>
+            <ModeToggle />
           </div>
         </div>
       </div>
