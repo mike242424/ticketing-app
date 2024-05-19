cb33aefada32bf00d692b82f8bc9edd6f74d6dd5
Michael Byers <mikebyers24@gmail.com>
2024-05-19 08:51:33 -0400

    server side pagination.

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -4,8 +4,18 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Pagination from '@/components/Pagination';
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+const Tickets = async ({
+  searchParams: { page },
+}: {
+  searchParams: { page: string };
+}) => {
+  const pageSize = 10;
+  const ticketCount = await prisma.ticket.count();
+
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (+page - 1) * pageSize,
+  });
 
   return (
     <div className="flex flex-col items-center">
@@ -13,7 +23,11 @@ const Tickets = async () => {
         <Button>New Ticket</Button>
       </Link>
       <TicketsTable tickets={tickets} />
-      <Pagination itemCount={26} pageSize={10} currentPage={1} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={+page || 1}
+      />
     </div>
   );
 };
