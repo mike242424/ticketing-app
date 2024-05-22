9ea3abed2c7d8ed23c55d7e9c5ba7c0be150900f
Michael Byers <mikebyers24@gmail.com>
2024-05-22 12:44:27 -0400

    created prisma user model and migrated to mySQL

---
+++ prisma/migrations/20240522164314_user_table/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE `Ticket` ADD COLUMN `assignedToUserId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'TECH', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
